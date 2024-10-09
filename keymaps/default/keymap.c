23028b1649b00423d0af9791b4c8938a3a485f04
justNeto <continental.lelgg.2b@gmail.com>
2024-10-09 01:33:43 -0600

    Testing if MT works

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     *
+     * Base Layout
      * ┌───┬───┬───┬───┬───┬───┬───┐
      * │ A │ B │ C │ D │ E │ F │ G │
      * ├───┼───┼───┼───┼───┼───┼───┤
@@ -27,13 +27,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [0] = LAYOUT(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
-                          KC_C,    KC_D,
-                          KC_C,    KC_D,
-                          KC_C,    KC_D,
-                          KC_C,    KC_D
+        KC_T,               KC_1,       KC_2,           KC_3,       KC_4,    KC_5,    KC_AUDIO_VOL_UP,
+        KC_TILD,            KC_SCLN,    KC_COMMA,       KC_DOT,     KC_P,    KC_Y,    KC_AUDIO_VOL_DOWN,
+        KC_A,               KC_A,       KC_O,           KC_E,       KC_U,    KC_I,    KC_AUDIO_MUTE,
+        KC_LEFT_SHIFT,      KC_QUOTE,    KC_Q,          KC_J,       KC_K,    KC_X,    KC_T,
+                          KC_T,    KC_T,
+                          MT(MOD_LGUI, KC_ESCAPE),    KC_T,
+                          KC_SPACE,    KC_T,
+                          MT(MOD_LCTL, KC_TAB),    KC_DELETE
     )
 };
