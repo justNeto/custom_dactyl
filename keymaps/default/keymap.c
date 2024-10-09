eab8849906e0d1f882d39b919eb07c36ebbee830
justNeto <continental.lelgg.2b@gmail.com>
2024-10-09 01:16:51 -0600

    Added base config for custom dactyl manuform keyboard

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,39 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     *
+     * ┌───┬───┬───┬───┬───┬───┬───┐
+     * │ A │ B │ C │ D │ E │ F │ G │
+     * ├───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │
+     * ├───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │
+     * ├───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │
+     * └───┴───┼───┼───┼───┴───┴───┘
+     *         │ C │ D │
+     *         ├───┼───┤
+     *         │ C │ D │
+     *         ├───┼───┤
+     *         │ C │ D │
+     *         ├───┼───┤
+     *         │ C │ D │
+     *         └───┴───┘
+     *
+     */
+
+    [0] = LAYOUT(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,
+                          KC_C,    KC_D,
+                          KC_C,    KC_D,
+                          KC_C,    KC_D,
+                          KC_C,    KC_D
+    )
+};
