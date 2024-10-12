f3ec48155a077432c0d5ae2b768dfa9b7efacc68
justNeto <continental.lelgg.2b@gmail.com>
2024-10-12 15:18:39 -0600

    Added a simple layout for testing split keyboard

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -32,16 +32,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [0] = LAYOUT(
-        KC_NO,          KC_1,           KC_2,           KC_3,       KC_4,    KC_5,      KC_PRINT_SCREEN,
-        KC_NO,          KC_SCLN,        KC_COMMA,       KC_DOT,     KC_P,    KC_Y,      KC_AUDIO_VOL_UP,
-        TG(2),          KC_A,           KC_O,           KC_E,       KC_U,    KC_I,      KC_AUDIO_VOL_DOWN,
-        KC_LEFT_SHIFT,  KC_QUOTE,       KC_Q,           KC_J,       KC_K,    KC_X,      KC_AUDIO_MUTE,
-                        TG(3),          TG(4),
+        KC_T,           KC_1,               KC_2,           KC_3,       KC_4,    KC_5,      KC_PRINT_SCREEN,                        KC_T,          KC_6,            KC_7,           KC_8,       KC_9,           KC_0,      KC_T,
+        KC_T,           KC_SCLN,            KC_COMMA,       KC_DOT,     KC_P,    KC_Y,      KC_AUDIO_VOL_UP,                        KC_T,          KC_F,            KC_G,           KC_C,       KC_R,           KC_L,      KC_T,
+        KC_T,           KC_A,               KC_O,           KC_E,       KC_U,    KC_I,      KC_AUDIO_VOL_DOWN,                      KC_T,          KC_D,            KC_H,           KC_T,       KC_N,           KC_S,      KC_T,
+        KC_LEFT_SHIFT,  KC_QUOTE,           KC_Q,           KC_J,       KC_K,    KC_X,      KC_AUDIO_MUTE,                          KC_T,          KC_B,            KC_M,           KC_W,       KC_V,           KC_Z,      KC_T
+                        KC_T,               KC_T,                                                                                   KC_T,          KC_T,
 
-                        MT(MOD_LGUI, KC_ESCAPE),    LT(1, KC_SPACE),
-                        TG(1),                      KC_LEFT_SHIFT,
-                        MT(MOD_LCTL, KC_TAB),       KC_DELETE
+                        MT(MOD_LGUI, KC_ESCAPE),    KC_SPACE,                                                                                                                                   KC_BACKSPACE,   KC_ENTER,
+                        KC_T,                       KC_LEFT_SHIFT,                                                                                                                              KC_T,           KC_T,
+                        MT(MOD_LCTL, KC_TAB),       KC_DELETE,                                                                                                                                  KC_T,           KC_CAPS_LOCK
     ),
+
+    /* [0] = LAYOUT( */
+    /*     KC_T,          KC_1,           KC_2,           KC_3,       KC_4,    KC_5,      KC_PRINT_SCREEN,                     KC_T,          KC_6,            KC_7,           KC_8,       KC_9,    KC_0,      KC_T, */
+    /*     KC_T,          KC_SCLN,        KC_COMMA,       KC_DOT,     KC_P,    KC_Y,      KC_AUDIO_VOL_UP,                     KC_T,          KC_F,            KC_G,           KC_C,       KC_R,    KC_L,      KC_T, */
+    /*     TG(2),          KC_A,           KC_O,           KC_E,       KC_U,    KC_I,      KC_AUDIO_VOL_DOWN,                  KC_T,          KC_D,            KC_H,           KC_T,       KC_N,    KC_S,      KC_T, */
+    /*     KC_LEFT_SHIFT,  KC_QUOTE,       KC_Q,           KC_J,       KC_K,    KC_X,      KC_AUDIO_MUTE,                      KC_T,          KC_B,            KC_M,           KC_W,       KC_V,    KC_Z,      KC_T */
+    /*                     TG(3),          TG(4),                                                                              KC_T,          KC_NO, */
+    /**/
+    /*                     MT(MOD_LGUI, KC_ESCAPE),    LT(1, KC_SPACE),                                                                    KC_BACKSPACE,               KC_ENTER, */
+    /*                     TG(1),                      KC_LEFT_SHIFT,                                                                      KC_T,                       KC_T, */
+    /*                     MT(MOD_LCTL, KC_TAB),       KC_DELETE,                                                                          KC_T,       KC_CAPS_LOCK */
+    /* ), */
 
     /*
      * Special chars (programming)
@@ -67,22 +79,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *      *: Tab(tap)/Ctrl(held)
      */
 
-    [1] = LAYOUT(
-        KC_NO,              KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-        KC_NO,              KC_COMMA,           KC_GRAVE,       KC_AMPR,        KC_LBRC,        KC_LABK,        KC_TRANSPARENT,
-        KC_0,               KC_COLN,            KC_QUOTE,       KC_LPRN,        KC_LCBR,        KC_PLUS,        KC_TRANSPARENT,
-        KC_TRANSPARENT,     KC_DOT,             KC_MINUS,       KC_HASH,        KC_CIRC,        KC_AT,          KC_TRANSPARENT,
-                            KC_TRANSPARENT,     KC_TRANSPARENT,
-
-                            KC_TRANSPARENT,     KC_TRANSPARENT,
-                            KC_TRANSPARENT,     KC_TRANSPARENT,
-                            KC_TRANSPARENT,     KC_TRANSPARENT
-    ),
+    /* [1] = LAYOUT( */
+    /*     KC_NO,              KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, */
+    /*     KC_NO,              KC_COMMA,           KC_GRAVE,       KC_AMPR,        KC_LBRC,        KC_LABK,        KC_TRANSPARENT, */
+    /*     KC_0,               KC_COLN,            KC_QUOTE,       KC_LPRN,        KC_LCBR,        KC_PLUS,        KC_TRANSPARENT, */
+    /*     KC_TRANSPARENT,     KC_DOT,             KC_MINUS,       KC_HASH,        KC_CIRC,        KC_AT,          KC_TRANSPARENT, */
+    /*                         KC_TRANSPARENT,     KC_TRANSPARENT, */
+    /**/
+    /*                         KC_TRANSPARENT,     KC_TRANSPARENT, */
+    /*                         KC_TRANSPARENT,     KC_TRANSPARENT, */
+    /*                         KC_TRANSPARENT,     KC_TRANSPARENT */
+    /* ), */
 
     /*
      * Number + F keys + arrows layer
      */
-    [2] = LAYOUT(
+    /* [2] = LAYOUT(
         KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
         KC_TRANSPARENT,     KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,
         KC_TRANSPARENT,     KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,
@@ -92,12 +104,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_TRANSPARENT,     KC_TRANSPARENT,
                             KC_TRANSPARENT,     KC_TRANSPARENT,
                             KC_TRANSPARENT,     KC_TRANSPARENT
-    ),
+    ) */,
 
     /*
      * League of Legends gaming layer
      */
-    [3] = LAYOUT(
+    /* [3] = LAYOUT(
         KC_NO,              KC_1,               KC_2,           KC_3,           KC_4,           KC_5,           KC_P,
         KC_TAB,             KC_Q,               KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,
         KC_LEFT_GUI,        KC_A,               KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,
@@ -107,14 +119,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_TRANSPARENT,     LT(3, KC_SPACE),
                             KC_TRANSPARENT,     KC_TRANSPARENT,
                             KC_TRANSPARENT,     KC_TRANSPARENT
-    ),
+    ) */,
 
     /*
      *
      * General gaming layer
      *
      */
-    [4] = LAYOUT(
+    /* [4] = LAYOUT(
         KC_NO,          KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,
         KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,
         KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,
@@ -124,5 +136,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         KC_TRANSPARENT,     KC_TRANSPARENT,
                                         KC_TRANSPARENT,     KC_TRANSPARENT,
                                         KC_TRANSPARENT,     KC_TRANSPARENT
-    ),
+    ) */,
 };
